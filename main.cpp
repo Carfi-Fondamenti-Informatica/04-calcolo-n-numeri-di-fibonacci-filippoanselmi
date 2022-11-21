3c182dd367bca3ec93eaf9f835d70c5b6dac5cc6
filippoanselmi <116039968+filippoanselmi@users.noreply.github.com>
2022-11-21 22:53:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   int n=0; cin >> n;
+   int somma=1, x=1;
+   int i=0;
+   if (n<2) {
+      cout << "errore";
+   } else {
+      while (i<n) {
+         cout << somma << endl;
+         somma+=x;
+         i++;
+         if (i<n) {
+            cout << x << endl;
+            x+=somma;
+            i++;
+         } else {
+            break;
+         }
+      }
+   }
    return 0;
 }
